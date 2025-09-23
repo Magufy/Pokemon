fa7f3867b71344eae9d3162c00b94665a383f35c
Magufy <martin.champey@gmail.com>
2025-09-23 23:58:03 +0200

    Update BotBienBoosté.py

--- JEU/BotBienBoosté.py
+++ JEU/BotBienBoosté.py
@@ -3,14 +3,14 @@ from random import choice
 import copy
 
 class Bot:
-    def __init__(self,pokemons_dispo):
+    def __init__(self,pokemons_dispo:list)-> None:
         self.equipe_bot=[]
         for i in range (6) :
             self.equipe_bot.append(copy.deepcopy(random.choice(pokemons_dispo)))
         self.poke_front_bot=self.equipe_bot[0]
         self.equipe_bot.remove(self.poke_front_bot)
 
-    def choix_pokemon_bot(self):
+    def choix_pokemon_bot(self)-> None:
 
         poke_front_avant=self.poke_front_bot
 
@@ -21,3 +21,4 @@ class Bot:
             self.equipe_bot.append(poke_front_avant)
         
         print(f"Le bot rappelle {poke_front_avant.nom}et fait entrer {self.poke_front_bot.nom}")
+
